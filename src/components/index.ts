e68dfa1ea03f62e8d0e1fa191e3da9f4e285c5a9
Miguel Colmenarez <m.colmenarez.dev@gmail.com>
2024-05-16 16:01:42 -0400

    feat: :tada: first commit

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+export { default as Blogdemo } from './Blogdemo.astro';
+export { default as Button } from './Button.astro'
+export { default as Card } from './Card.astro'
+export { default as Footer } from './Footer.astro'
+export { default as Navbar } from './Navbar.astro'
+export { default as Section } from './Section.astro'
+export { default as Testimonials } from './Testimonials.astro'
